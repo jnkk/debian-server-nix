b152627bd96e59227e085aad4bef403c65d2be04
Arian Reza <arian.reza.setiawan@gmail.com>
2025-05-14 23:56:07 +0700

    lazygit, neovim

--- home.nix
+++ home.nix
@@ -17,7 +17,10 @@
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
-  home.packages = [
+  home.packages = with pkgs; [
+
+    lazygit
+    neovim
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     # pkgs.hello
